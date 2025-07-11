bcd89900292179dc17b91e78370bf8a560bb91ae
anamnikhilesh@gmail.com <anamnikhilesh@gmail.com>
2025-07-11 11:03:00 -0700

    Initial Commit

---
+++ TipCalculator/TipCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipCalculatorApp.swift
+//  TipCalculator
+//
+//  Created by NIKHILESH KUMAR REDDY ANAM on 7/11/25.
+//
+
+import SwiftUI
+
+@main
+struct TipCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TipCalculatorTests/TipCalculatorTests.swift
@@ -0,0 +1,17 @@
+//
+//  TipCalculatorTests.swift
+//  TipCalculatorTests
+//
+//  Created by NIKHILESH KUMAR REDDY ANAM on 7/11/25.
+//
+
+import Testing
+@testable import TipCalculator
+
+struct TipCalculatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
